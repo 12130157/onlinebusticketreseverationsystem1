3aea14b8f4c202da8303b62fbadcbd5c109c8821
thanhdvv <thanhdvv@f6fd0471-4acc-a005-63d4-b7f77fb58ca9>
2010-10-09 09:36:08 +0000

    upload website

---
+++  onlinebusticketreseverationsystem1/OnlineBusTicketReseveration/BOL/Entity/BusType.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace BOL.Entity
+{
+    class BusType
+    {
+        private int bt_id;
+
+        public int Bt_Id
+        {
+            get { return bt_id; }
+            set { bt_id = value; }
+        }
+        private String  name;
+
+        public String  Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+        private String description;
+
+        public String Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+        
+    }
+}
